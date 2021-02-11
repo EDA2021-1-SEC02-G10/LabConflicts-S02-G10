ffb840d963524dd34f847444d2d709fdd02360fb
Julipa02 <j.parraf@uniandes.edu.co>
2021-02-10 19:05:52 -0500

    Cambios del estudiante 1

--- App/model.py
+++ App/model.py
@@ -76,4 +76,3 @@ def addBookTags(booktagsfile):
     # TO-DO: Modificación de Est-1 y Est-2 en el Lab 2
     booktags = lt.newList(datastructure='SINGLE_LINKED', filename=booktagsfile)
     return booktags
-    pass
